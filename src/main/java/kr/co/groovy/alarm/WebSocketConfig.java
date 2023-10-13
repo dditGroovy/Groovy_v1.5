848df5d30def359a2802d52d32bb0a3ba8def11f
ChoiDazzi <choidazzi@gmail.com>
2023-10-13 18:39:12 +0900

    socket 수정

---
+++ src/main/java/kr/co/groovy/alarm/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package kr.co.groovy.alarm;
+
+import kr.co.groovy.employee.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private EmployeeService service;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/echo-ws")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("https://12fa-175-116-155-226.ngrok-free.app/")
+                .withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new AlarmHandler(service);
+    }
+
+    // websocket 관련 설정
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
+    }
+
+}
